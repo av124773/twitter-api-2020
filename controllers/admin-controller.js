0ff9718b85620799d4bb3fe3d880b6cca6fa7291
jiasyuanchu <jiasyuanchu@gmail.com>
2023-06-08 16:11:23 +0800

    feat: set up routers and controllers

---
+++ controllers/admin-controller.js
@@ -0,0 +1,7 @@
+const adminController = {
+  getAdmin: (req, res, next) => {
+    res.send('hello world!') // this is for route testing
+  }
+}
+
+module.exports = adminController
---
+++ controllers/tweet-controller.js
@@ -0,0 +1,7 @@
+const tweetController = {
+  getTweet: (req, res, next) => {
+    res.send('hello world!') // this is for route testing
+  }
+}
+
+module.exports = tweetController
