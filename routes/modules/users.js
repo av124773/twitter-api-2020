ddcc5162d002f6417a18ef544567ca7e689d7fa9
jiasyuanchu <jiasyuanchu@gmail.com>
2023-06-10 14:02:14 +0800

    fix: signin function fixed for admin/user

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
+const passport = require('../../config/passport')
 
 const userController = require('../../controllers/user-controller')
 const { isUser, authenticatedUser } = require('../../middleware/auth')
-
-router.post('/login', passport.authenticate('local', { session: false }), isUser, userController.signIn)
 
 module.exports = router
